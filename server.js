227cf7993ca96679ba1bf87a5fb57c2eab09a01a
Glitch (notch-temperature) <none>
2018-11-30 16:11:06 +0000

    🛩⛪️ Checkpoint ./server.js:318686/268

--- server.js
+++ server.js
@@ -25,24 +25,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-// Not found middleware
-// app.use((req, res, next) => {
-//   return next({status: 404, message: 'not found'})
-// })
-
-// Error Handling middleware
 app.use((err, req, res, next) => {
   let errCode, errMessage
 
   if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
+    errCode = 400
     const keys = Object.keys(err.errors)
-    // report the first validation error
     errMessage = err.errors[keys[0]].message
   } else {
-    // generic or custom error
     errCode = err.status || 500
     errMessage = err.message || 'Internal Server Error'
   }
